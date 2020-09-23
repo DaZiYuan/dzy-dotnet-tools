de3f9ec14143567925ab62aa4f10236b05e2ff7b
DaZiYuan <mscoder@hotmail.com>
2020-09-23 17:31:10 +0800

    迁移winapi

---
+++ src/WinAPI/DwmapiWrapper.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace DZY.WinAPI
+{
+    [Flags]
+    public enum BbFlags : byte //Blur Behind Flags
+    {
+        DwmBbEnable = 1,
+        DwmBbBlurregion = 2,
+        DwmBbTransitiononmaximized = 4,
+    };
+    [StructLayout(LayoutKind.Sequential)]
+    public struct BbStruct //Blur Behind Structure
+    {
+        public BbFlags Flags;
+        public bool Enable;
+        public IntPtr Region;
+        public bool TransitionOnMaximized;
+    }
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Margins
+    {
+        public int cxLeftWidth;      // width of left border that retains its size
+        public int cxRightWidth;     // width of right border that retains its size
+        public int cyTopHeight;      // height of top border that retains its size
+        public int cyBottomHeight;   // height of bottom border that retains its size
+    };
+    [StructLayout(LayoutKind.Sequential)]
+    public struct DWM_COLORIZATION_PARAMS
+    {
+        public int clrColor;
+        public int clrAfterGlow;
+        public int nIntensity;
+        public int clrAfterGlowBalance;
+        public int clrBlurBalance;
+        public int clrGlassReflectionIntensity;
+        public bool fOpaque;
+    }
+    public class DwmapiWrapper
+    {
+
+        [DllImport("dwmapi.dll")]
+        public static extern int DwmEnableBlurBehindWindow(IntPtr hWnd, ref BbStruct blurBehind);
+
+        [DllImport("DwmApi.dll")]
+        public static extern int DwmExtendFrameIntoClientArea(IntPtr hwnd, ref Margins pMarInset);
+
+        [DllImport("dwmapi.dll", EntryPoint = "#127", PreserveSig = false)]
+        public static extern void DwmGetColorizationParameters(out DWM_COLORIZATION_PARAMS parameters);
+
+        [DllImport("dwmapi.dll", PreserveSig = false)]
+        public static extern bool DwmIsCompositionEnabled();
+
+        [DllImport("dwmapi.dll")]
+        public static extern int DwmIsCompositionEnabled(out bool enabled);
+
+        [DllImport("dwmapi.dll", EntryPoint = "#131", PreserveSig = false)]
+        public static extern void DwmSetColorizationParameters(ref DWM_COLORIZATION_PARAMS parameters, long uUnknown);
+
+        [DllImport("dwmapi.dll")]
+        public static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
+
+        public enum DWMWINDOWATTRIBUTE
+        {
+            DWMWA_NCRENDERING_ENABLED = 1,
+            DWMWA_NCRENDERING_POLICY,
+            DWMWA_TRANSITIONS_FORCEDISABLED,
+            DWMWA_ALLOW_NCPAINT,
+            DWMWA_CAPTION_BUTTON_BOUNDS,
+            DWMWA_NONCLIENT_RTL_LAYOUT,
+            DWMWA_FORCE_ICONIC_REPRESENTATION,
+            DWMWA_FLIP3D_POLICY,
+            DWMWA_EXTENDED_FRAME_BOUNDS,
+            DWMWA_HAS_ICONIC_BITMAP,
+            DWMWA_DISALLOW_PEEK,
+            DWMWA_EXCLUDED_FROM_PEEK,
+            DWMWA_CLOAK,
+            DWMWA_CLOAKED,
+            DWMWA_FREEZE_REPRESENTATION,
+            DWMWA_LAST
+        }
+
+        [DllImport("dwmapi.dll")]
+        public static extern int DwmGetWindowAttribute(IntPtr Window, DWMWINDOWATTRIBUTE Attribute, out bool Value, int Size);
+    }
+}
---
+++ src/WinAPI/Kernel32Wrapper.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace DZY.WinAPI
+{
+    #region 结构体
+
+    /// <summary>
+    /// Process information from the create process call
+    /// Represents PROCESS_INFORMATION in win32
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct PROCESS_INFORMATION
+    {
+        /// <summary>
+        /// A handle to the newly created process. The handle is used to specify the process in all functions that perform operations on the process object.
+        /// </summary>
+        public IntPtr hProcess;
+
+        /// <summary>
+        /// A handle to the primary thread of the newly created process. The handle is used to specify the thread in all functions that perform operations on the thread object
+        /// </summary>
+        public IntPtr hThread;
+
+        /// <summary>
+        /// A value that can be used to identify a process.
+        /// The value is valid from the time the process is created until all handles to the process are closed and 
+        /// the process object is freed; at this point, the identifier may be reused.
+        /// </summary>
+        public int dwProcessId;
+
+        /// <summary>
+        /// A value that can be used to identify a thread. The value is valid from the time the thread is created until all handles to the thread are closed and the thread object is freed; at this point, the identifier may be reused.
+        /// </summary>
+        public int dwThreadId;
+    }
+    /// <summary>
+    /// Structure that contains the startupinfo
+    /// Represents STARTUP_INFO in win32
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    public struct STARTUP_INFO
+    {
+        /// <summary>
+        /// The size of the structure, in bytes.
+        /// </summary>
+        public Int32 cb;
+
+        /// <summary>
+        /// Reserved; must be NULL
+        /// </summary>
+        public string lpReserved;
+
+        /// <summary>
+        /// The name of the desktop, or the name of both the desktop and window station for this process.
+        /// A backslash in the string indicates that the string includes both the desktop and window station names
+        /// </summary>
+        public string lpDesktop;
+
+        /// <summary>
+        /// For console processes, this is the title displayed in the title bar if a new console window is created. 
+        /// If NULL, the name of the executable file is used as the window title instead. 
+        /// This parameter must be NULL for GUI or console processes that do not create a new console window
+        /// </summary>
+        public string lpTitle;
+
+        /// <summary>
+        /// If dwFlags specifies STARTF_USEPOSITION, this member is the x offset of the upper left corner of a window if a new window is created, in pixels. Otherwise, this member is ignored
+        /// </summary>
+        public Int32 dwX;
+
+        /// <summary>
+        /// If dwFlags specifies STARTF_USEPOSITION, this member is the y offset of the upper left corner of a window if a new window is created, in pixels. Otherwise, this member is ignored.
+        /// </summary>
+        public Int32 dwY;
+
+        /// <summary>
+        /// If dwFlags specifies STARTF_USESIZE, this member is the width of the window if a new window is created, in pixels. Otherwise, this member is ignored. 
+        /// </summary>
+        public Int32 dwXSize;
+
+        /// <summary>
+        /// If dwFlags specifies STARTF_USESIZE, this member is the height of the window if a new window is created, in pixels. Otherwise, this member is ignored.
+        /// </summary>
+        public Int32 dwYSize;
+
+        /// <summary>
+        /// If dwFlags specifies STARTF_USECOUNTCHARS, if a new console window is created in a console process, this member specifies the screen buffer width, in character columns. Otherwise, this member is ignored.
+        /// </summary>
+        public Int32 dwXCountChars;
+
+        /// <summary>
+        /// If dwFlags specifies STARTF_USECOUNTCHARS, if a new console window is created in a console process, this member specifies the screen buffer height, in character rows. Otherwise, this member is ignored.dwFillAttribute 
+        /// </summary>
+        public Int32 dwYCountChars;
+
+        /// <summary>
+        /// If dwFlags specifies STARTF_USEFILLATTRIBUTE, this member is the initial text and background colors if a new console window is created in a console application. Otherwise, this member is ignored. 
+        /// </summary>
+        public Int32 dwFillAttribute;
+
+        /// <summary>
+        /// A bit field that determines whether certain STARTUPINFO members are used when the process creates a window
+        /// </summary>
+        public Int32 dwFlags;
+
+        /// <summary>
+        /// If dwFlags specifies STARTF_USESHOWWINDOW, this member can be any of the SW_ constants defined in Winuser.h. Otherwise, this member is ignored.
+        /// </summary>
+        public Int16 wShowWindow;
+
+        /// <summary>
+        /// Reserved for use by the C Run-time; must be zero.
+        /// </summary>
+        public Int16 cbReserved2;
+
+        /// <summary>
+        /// Reserved for use by the C Run-time; must be NULL.
+        /// </summary>
+        public IntPtr lpReserved2;
+
+        /// <summary>
+        /// If dwFlags specifies STARTF_USESTDHANDLES, this member is the standard input handle for the process. Otherwise, this member is ignored and the default for standard input is the keyboard buffer.
+        /// </summary>
+        public IntPtr hStdInput;
+
+        /// <summary>
+        /// If dwFlags specifies STARTF_USESTDHANDLES, this member is the standard output handle for the process. Otherwise, this member is ignored and the default for standard output is the console window's buffer.
+        /// </summary>
+        public IntPtr hStdOutput;
+
+        /// <summary>
+        /// If dwFlags specifies STARTF_USESTDHANDLES, this member is the standard error handle for the process. Otherwise, this member is ignored and the default for standard error is the console window's buffer.
+        /// </summary>
+        public IntPtr hStdError;
+    }
+    /// <summary>
+    /// Structure to contain security attributes from the create process call represents
+    /// SECURITY_ATTRIBUTE in win32
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SECURITY_ATTRIBUTES
+    {
+        /// <summary>
+        /// The size, in bytes, of this structure. Set this value to the size of the SECURITY_ATTRIBUTES structure
+        /// </summary>
+        public int nLength;
+
+        /// <summary>
+        /// A pointer to a security descriptor for the object that controls the sharing of it.
+        /// If NULL is specified for this member, the object is assigned the default security descriptor of the calling process. 
+        /// This is not the same as granting access to everyone by assigning a NULL discretionary access control list (DACL). 
+        /// The default security descriptor is based on the default DACL of the access token belonging to the calling process.
+        /// By default, the default DACL in the access token of a process allows access only to the user represented by the access token. 
+        /// If other users must access the object, you can either create a security descriptor with the appropriate access, 
+        /// or add ACEs to the DACL that grants access to a group of users. 
+        /// </summary>
+        public IntPtr lpSecurityDescriptor;
+
+        /// <summary>
+        /// A Boolean value that specifies whether the returned handle is inherited when a new process is created.
+        /// If this member is TRUE, the new process inherits the handle.
+        /// </summary>
+        public int bInheritHandle;
+    }
+    [Flags]
+    public enum ThreadAccess : int
+    {
+        TERMINATE = (0x0001),
+        SUSPEND_RESUME = (0x0002),
+        GET_CONTEXT = (0x0008),
+        SET_CONTEXT = (0x0010),
+        SET_INFORMATION = (0x0020),
+        QUERY_INFORMATION = (0x0040),
+        SET_THREAD_TOKEN = (0x0080),
+        IMPERSONATE = (0x0100),
+        DIRECT_IMPERSONATION = (0x0200)
+    }
+    [StructLayout(LayoutKind.Sequential)]
+    public struct IO_COUNTERS
+    {
+        public UInt64 ReadOperationCount;
+        public UInt64 WriteOperationCount;
+        public UInt64 OtherOperationCount;
+        public UInt64 ReadTransferCount;
+        public UInt64 WriteTransferCount;
+        public UInt64 OtherTransferCount;
+    }
+    [Flags]
+    public enum JOBOBJECTLIMIT : uint
+    {
+        JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE = 0x2000
+    }
+    [StructLayout(LayoutKind.Sequential)]
+    public struct JOBOBJECT_BASIC_LIMIT_INFORMATION
+    {
+        public Int64 PerProcessUserTimeLimit;
+        public Int64 PerJobUserTimeLimit;
+        public JOBOBJECTLIMIT LimitFlags;
+        public UIntPtr MinimumWorkingSetSize;
+        public UIntPtr MaximumWorkingSetSize;
+        public UInt32 ActiveProcessLimit;
+        public Int64 Affinity;
+        public UInt32 PriorityClass;
+        public UInt32 SchedulingClass;
+    }
+    [StructLayout(LayoutKind.Sequential)]
+    public struct JOBOBJECT_EXTENDED_LIMIT_INFORMATION
+    {
+        public JOBOBJECT_BASIC_LIMIT_INFORMATION BasicLimitInformation;
+        public IO_COUNTERS IoInfo;
+        public UIntPtr ProcessMemoryLimit;
+        public UIntPtr JobMemoryLimit;
+        public UIntPtr PeakProcessMemoryUsed;
+        public UIntPtr PeakJobMemoryUsed;
+    }
+    public enum JobObjectInfoType
+    {
+        AssociateCompletionPortInformation = 7,
+        BasicLimitInformation = 2,
+        BasicUIRestrictions = 4,
+        EndOfJobTimeInformation = 6,
+        ExtendedLimitInformation = 9,
+        SecurityLimitInformation = 5,
+        GroupInformation = 11
+    }
+
+    #endregion
+    public class Kernel32Wrapper
+    {
+        [DllImport("kernel32.dll")]
+        public static extern bool SetProcessWorkingSetSize(IntPtr handle, int minimumWorkingSetSize, int maximumWorkingSetSize);
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetConsoleWindow();
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+        /// <summary>
+        /// Create a new process
+        /// </summary>
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool CreateProcess(
+            string lpApplicationName,
+            string lpCommandLine,
+            ref SECURITY_ATTRIBUTES lpProcessAttributes,
+            ref SECURITY_ATTRIBUTES lpThreadAttributes,
+            [In, MarshalAs(UnmanagedType.Bool)]
+            bool bInheritHandles,
+            uint dwCreationFlags,
+            IntPtr lpEnvironment,
+            string lpCurrentDirectory,
+            [In] ref STARTUP_INFO lpStartupInfo,
+            out PROCESS_INFORMATION lpProcessInformation
+        );
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenThread(ThreadAccess dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
+        [DllImport("kernel32.dll")]
+        public static extern uint SuspendThread(IntPtr hThread);
+        [DllImport("kernel32.dll")]
+        public static extern int ResumeThread(IntPtr hThread);
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        public static extern IntPtr CreateJobObject(object a, string lpName);
+        [DllImport("kernel32.dll")]
+        public static extern bool SetInformationJobObject(IntPtr job, JobObjectInfoType infoType, IntPtr lpJobObjectInfo, uint cbJobObjectInfoLength);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool AssignProcessToJobObject(IntPtr job, IntPtr process);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool CloseHandle(IntPtr hObject);
+    }
+}
