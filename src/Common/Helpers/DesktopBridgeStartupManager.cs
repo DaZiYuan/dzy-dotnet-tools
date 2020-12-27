d6d19130e6674433c5f59bb61b001fe49aeb8a6a
DaZiYuan <mscoder@hotmail.com>
2020-12-27 11:27:05 +0800

    开机启动问题

--- src/Common/Helpers/DesktopBridgeStartupManager.cs
+++ src/Common/Helpers/DesktopBridgeStartupManager.cs
@@ -47,6 +47,7 @@ namespace Common.Helpers
             }
             catch (Exception ex)
             {
+                System.Diagnostics.Debug.WriteLine(ex);
                 return false;
             }
         }
