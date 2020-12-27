bfb37ce926e63ff15e1c5a53c387e305de6bb4b5
DaZiYuan <mscoder@hotmail.com>
2020-12-27 22:23:40 +0800

    发布

---
+++ src/Common/Helpers/ProcessHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Common.Helpers
+{
+    public class ProcessHelper
+    {
+        public static void AddPathToEnvoirment(string dir)
+        {
+            var name = "PATH";
+            var scope = EnvironmentVariableTarget.Process;
+            var oldValue = Environment.GetEnvironmentVariable(name, scope);
+            if (oldValue != null && dir != null && oldValue.Contains(dir))
+                //已经有了
+                return;
+            var newValue = oldValue + $";{dir}";
+            Environment.SetEnvironmentVariable(name, newValue, scope);
+        }
+
+        public static void RemovePathFromEnviorment(string dir)
+        {
+            var name = "PATH";
+            var scope = EnvironmentVariableTarget.Process;
+            var oldValue = Environment.GetEnvironmentVariable(name, scope);
+            var newValue = oldValue.Replace(dir, "");
+            Environment.SetEnvironmentVariable(name, newValue, scope);
+        }
+    }
+}
