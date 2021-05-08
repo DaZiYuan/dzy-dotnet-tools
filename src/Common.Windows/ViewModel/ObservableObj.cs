6787371cdf7327a4879e1bc9010bd37d811e2f72
DaZiYuan <mscoder@hotmail.com>
2021-05-08 14:24:16 +0800

    发布

---
+++ src/Common.Windows/ViewModel/ObservableObj.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace Common.Windows.ViewModel
+{
+    public class ObservableObj : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyOfPropertyChange(string propertyName)
+        {
+            var handle = PropertyChanged;
+            if (handle == null)
+                return;
+            handle(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
