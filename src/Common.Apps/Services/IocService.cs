bf94f5fead311743f26b9f738033bfad2faed4ad
DaZiYuan <mscoder@hotmail.com>
2022-05-18 18:16:35 +0800

    增加getservice

--- src/Common.Apps/Services/IocService.cs
+++ src/Common.Apps/Services/IocService.cs
@@ -24,6 +24,11 @@ namespace Common.Apps.Services
             Services = ConfigureServices(serviceOption, services);
         }
 
+        public static T? GetService<T>()
+        {
+            return Services.GetService<T>();
+        }
+
 #pragma warning disable CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑声明为可以为 null。
         public static IServiceProvider Services { get; private set; }
 #pragma warning restore CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑声明为可以为 null。
