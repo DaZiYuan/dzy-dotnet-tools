689e1d3a425066692df1ed1d870fc24aebb4a156
DaZiYuan <mscoder@hotmail.com>
2022-05-20 12:41:28 +0800

    对外简化

--- src/Common.Apps/Services/IocService.cs
+++ src/Common.Apps/Services/IocService.cs
@@ -15,7 +15,7 @@ namespace Common.Apps.Services
     //https://docs.microsoft.com/zh-cn/windows/communitytoolkit/mvvm/Ioc
     public class IocService
     {
-        public IocService()
+        private IocService()
         {
 
         }
@@ -26,12 +26,10 @@ namespace Common.Apps.Services
 
         public static T? GetService<T>()
         {
-            return Services.GetService<T>();
+            return Services!.GetService<T>();
         }
 
-#pragma warning disable CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑声明为可以为 null。
-        public static IServiceProvider Services { get; private set; }
-#pragma warning restore CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑声明为可以为 null。
+        private static IServiceProvider? Services;
 
         private static IServiceProvider ConfigureServices(InitServiceOption serviceOption, ServiceCollection services)
         {
